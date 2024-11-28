7f5c49c489d49531490dceeed011fbcd516f44bc
Geraldo Luiz <geraldobl58@gmail.com>
2024-11-28 11:35:36 -0300

    feat: Add authentication page

--- src/app/(routes)/(auth)/page.tsx
+++ src/app/(routes)/(auth)/page.tsx
@@ -1,7 +1,112 @@
+'use client'
+
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { Heading } from '@/components/heading'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { authFormSchema, AuthFormValues } from '@/modules/auth/schemas/auth'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { userLogin } from '@/modules/auth/services/auth'
+
 const AuthPage = () => {
+  const [loading, setLoading] = useState(false)
+
+  const form = useForm<AuthFormValues>({
+    resolver: zodResolver(authFormSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+  const onSubmit = async (data: AuthFormValues) => {
+    try {
+      setLoading(true)
+      const response = await userLogin(data)
+      console.log(response.data)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <div>
-      <div>AuthPage</div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="min-w-[700px] mx-auto">
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              <Heading
+                title="Login"
+                description="Realize seu login para acessar o sistema."
+              />
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8 w-full"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>E-mail</FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={loading}
+                          placeholder="Digite o e-mail"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Senha</FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={loading}
+                          placeholder="Digite a senha"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  type="submit"
+                  size="sm"
+                  disabled={loading}
+                  className="w-full"
+                >
+                  Login
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   )
 }
