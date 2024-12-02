ce6221fd4da97a7b5155c36ccd8f3f0c887d961f
Geraldo Luiz <geraldobl58@gmail.com>
2024-12-02 11:06:07 -0300

    test: Add test in filters users

--- src/app/(routes)/(app)/users/components/user-client/components/data-table/data-table-loading/test.tsx
+++ src/app/(routes)/(app)/users/components/user-client/components/data-table/data-table-loading/test.tsx
@@ -5,25 +5,25 @@ import { LoadingState } from '.'
 describe('LoadingState', () => {
   const mockColumns = [{}, {}, {}]
 
-  test('renders loading spinner', () => {
+  it('renders loading spinner', () => {
     render(<LoadingState columns={mockColumns} />)
     const spinner = screen.getByTestId('spinner')
     expect(spinner).toBeInTheDocument()
   })
 
-  test('renders loading text', () => {
+  it('renders loading text', () => {
     render(<LoadingState columns={mockColumns} />)
     const loadingText = screen.getByText('Carregando...')
     expect(loadingText).toBeInTheDocument()
   })
 
-  test('renders correct number of skeletons in table header', () => {
+  it('renders correct number of skeletons in table header', () => {
     render(<LoadingState columns={mockColumns} />)
     const headerSkeletons = screen.getAllByRole('columnheader')
     expect(headerSkeletons).toHaveLength(mockColumns.length)
   })
 
-  test('renders correct number of skeletons in table body', () => {
+  it('renders correct number of skeletons in table body', () => {
     render(<LoadingState columns={mockColumns} />)
     const bodySkeletons = screen.getAllByRole('cell')
     expect(bodySkeletons).toHaveLength(mockColumns.length * 5)
--- src/app/(routes)/(app)/users/components/user-client/components/data-table/data-table-pagination/test.tsx
+++ src/app/(routes)/(app)/users/components/user-client/components/data-table/data-table-pagination/test.tsx
@@ -18,34 +18,34 @@ describe('PaginationMeta', () => {
     )
   }
 
-  test('renders correctly', () => {
+  it('renders correctly', () => {
     renderComponent(1)
     expect(screen.getByText('Página 1 de 10')).toBeInTheDocument()
     expect(screen.getByText('Total: 100 registro(s)')).toBeInTheDocument()
   })
 
-  test('previous button is disabled on first page', () => {
+  it('previous button is disabled on first page', () => {
     renderComponent(1)
     expect(
       screen.getByRole('button', { name: /arrowlefticon/i })
     ).toBeDisabled()
   })
 
-  test('next button is disabled on last page', () => {
+  it('next button is disabled on last page', () => {
     renderComponent(10)
     expect(
       screen.getByRole('button', { name: /arrowrighticon/i })
     ).toBeDisabled()
   })
 
-  test('calls handleNextPage when next button is clicked', () => {
+  it('calls handleNextPage when next button is clicked', () => {
     renderComponent(1)
     const nextButton = screen.getByRole('button', { name: /arrowrighticon/i })
     fireEvent.click(nextButton)
     expect(handleNextPage).toHaveBeenCalledTimes(1)
   })
 
-  test('calls handlePreviousPage when previous button is clicked', () => {
+  it('calls handlePreviousPage when previous button is clicked', () => {
     renderComponent(2)
     const prevButton = screen.getByRole('button', { name: /arrowlefticon/i })
     fireEvent.click(prevButton)
