199dd650869c4820d0cd11ceef396cb9bde59e4d
Geraldo Luiz <geraldobl58@gmail.com>
2024-12-11 16:50:26 -0300

    feat: Refactor StatusBadge component to use memo and simplify status mapping

--- src/app/(routes)/(app)/orders/components/orders-client/components/data-table-badge/index.tsx
+++ src/app/(routes)/(app)/orders/components/orders-client/components/data-table-badge/index.tsx
@@ -1,4 +1,7 @@
+import { memo } from 'react'
+
 import { OrderStatus } from '@/enums'
+
 import { cn } from '@/lib/utils'
 
 export interface StatusBadgeProps {
@@ -6,46 +9,33 @@ export interface StatusBadgeProps {
   label: string
 }
 
-const StatusBadge = ({ bgColor, label }: StatusBadgeProps) => {
+const StatusBadge = memo(({ bgColor, label }: StatusBadgeProps) => {
   return (
     <span
       className={cn(
-        `
-          text-white
-          text-xs
-          font-semibold
-          flex
-          items-center
-          justify-center
-          w-[120px]
-          p-1
-          rounded-full
-        `,
+        'text-white text-xs font-semibold flex items-center justify-center w-[120px] p-1 rounded-full',
         `bg-${bgColor}`
       )}
     >
       {label}
     </span>
   )
+})
+
+StatusBadge.displayName = 'StatusBadge'
+
+const statusBadgeMap: Record<OrderStatus, StatusBadgeProps> = {
+  [OrderStatus.PENDING]: { bgColor: 'violet-500', label: 'Pendente' },
+  [OrderStatus.SHIPPED]: { bgColor: 'orange-500', label: 'Enviado' },
+  [OrderStatus.DELIVERED]: { bgColor: 'emerald-500', label: 'Entregue' },
+  [OrderStatus.CANCELED]: { bgColor: 'rose-500', label: 'Cancelado' }
 }
 
 export const RenderStatusBadge = ({ status }: { status: OrderStatus }) => {
-  const getStatusBadgeProps = (status: OrderStatus): StatusBadgeProps => {
-    switch (status) {
-      case OrderStatus.PENDING:
-        return { bgColor: 'violet-500', label: 'Pendente' }
-      case OrderStatus.SHIPPED:
-        return { bgColor: 'orange-500', label: 'Enviado' }
-      case OrderStatus.DELIVERED:
-        return { bgColor: 'emerald-500', label: 'Entregue' }
-      case OrderStatus.CANCELED:
-        return { bgColor: 'rose-500', label: 'Cancelado' }
-      default:
-        return { bgColor: 'gray-500', label: 'Desconhecido' }
-    }
+  const badgeProps = statusBadgeMap[status] || {
+    bgColor: 'gray-500',
+    label: 'Desconhecido'
   }
-
-  const badgeProps = getStatusBadgeProps(status)
 
   return <StatusBadge {...badgeProps} />
 }
