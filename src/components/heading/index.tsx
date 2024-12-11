689398aa698378927f0c5ebdbfb3fdaea895c44e
Geraldo Luiz <geraldobl58@gmail.com>
2024-12-11 16:42:33 -0300

    feat: Make description prop optional in Heading component

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,13 +1,15 @@
 interface HeadingProps {
   title: string
-  description: string
+  description?: string
 }
 
 export const Heading = ({ title, description }: HeadingProps) => {
   return (
     <div>
       <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
-      <h2 className="text-sm text-muted-foreground">{description}</h2>
+      <h2 className="text-sm text-muted-foreground">
+        {description && description}
+      </h2>
     </div>
   )
 }
